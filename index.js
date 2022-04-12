2a095cdd73ee7e5e55fcb3294a42341595140fbb
nicolas sztajnchamer <nicosteinjamer@gmail.com>
2022-04-12 13:17:34 -0300

    ejercicio complementario

--- index.js
+++ index.js
@@ -4,9 +4,3 @@ while (edad < 18) {
     edad = prompt("cuantos años tenes")
 }
 alert ("bienvenido" + " " + nombre)
-
-
-
-/*Tenes que permitir que te ingresen un nombre,su edad si esa edad es menor a 18 
-tiene que volver a pedirselo hasta que sea una edad razonable. y su dni
-si la edad es razonable tiene que salir un mensaje diciendo bienvenido + nombre.*/
