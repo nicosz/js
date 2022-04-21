e56057eddae8154bf07dd470e51983496ad24c48
nicolas sztajnchamer <nicosteinjamer@gmail.com>
2022-04-21 11:53:11 -0300

    arrays

--- index.js
+++ index.js
@@ -1,51 +1,75 @@
-// let nombre = parseInt(prompt("ingresa un numero"))
-// let edad = parseInt (prompt("ingresa otro numero"))
 
-// // while (edad < 18) {
-// //  edad = prompt("cuantos años tenes")
-// // }
+class persona{
+ constructor(nombre,apellido,edad){
+   
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.edad=edad;
+}
+}
 
-// suma(edad, nombre)
+let personas =[]
+let palabra = []
+while (palabra != "esc"){
 
+    let nombre = prompt("ingrese nombre");
+    let edad = parseInt(prompt("ingrese su edad"));
+    let apellido = prompt("ingrese su apellido");
+    let persona1 = new persona (nombre,apellido,edad)
+    personas.push(persona1)
+    palabra = prompt("ingrese esc para salir")
 
-// function suma (a, b){
+}
 
-//     let suma = a + b;
+console.log(personas)
+// let nico = [1,2,3,4,5,6,7]
+// nico.push(8)
 
-//     return console.log(suma)
+// for (let dario of nico){
+
+//     console.log(dario)
+
 // }
 
-// condicionales, bucles y funciones 
+// let persona = {nombre:"nico" , edad:22}
+
+// for (let nico in persona){
+
+//     console.log(nico, persona[nico])
+
+// }
 
 
-let usuario1 = " ";
 
-do {
-    let usuario = parseInt(prompt("ingrese la opcion deseada:\n promocion 1 \n promocion 2 \n promocion 3"))
-    if(usuario == 1){
-        alert("opcion 1")
-        precio(1)
-    }else if (usuario == 2){
-        alert("opcion 2")
-        precio(2)
-    }else if (usuario == 3){
-        alert("opcion 3")
-        precio(3)
-    }else {
-        alert("ingrese una opcion valida")
-    }
-     usuario1 = prompt("ingrese esc para salir o cualquier tecla para continuar")
-}while (usuario1 != "esc")
-alert("gracias por su reserva")
-function precio (a){
 
-    if (a==1){
-        alert("el precio es de $100")
-    }
-    else if (a==2){
-        alert("el precio es de $200")
-    }
-    else if (a==3){
-        alert("el precio es de $300")
-    }
-}
+// let usuario1 = " ";
+
+// do {
+//     let usuario = parseInt(prompt("ingrese la opcion deseada:\n promocion 1 \n promocion 2 \n promocion 3"))
+//     if(usuario == 1){
+//         alert("opcion 1")
+//         precio(1)
+//     }else if (usuario == 2){
+//         alert("opcion 2")
+//         precio(2)
+//     }else if (usuario == 3){
+//         alert("opcion 3")
+//         precio(3)
+//     }else {
+//         alert("ingrese una opcion valida")
+//     }
+//      usuario1 = prompt("ingrese esc para salir o cualquier tecla para continuar")
+// }while (usuario1 != "esc")
+// alert("gracias por su reserva")
+// function precio (a){
+
+//     if (a==1){
+//         alert("el precio es de $100")
+//     }
+//     else if (a==2){
+//         alert("el precio es de $200")
+//     }
+//     else if (a==3){
+//         alert("el precio es de $300")
+//     }
+// }
